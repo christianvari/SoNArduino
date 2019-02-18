4a932814a6abada31bfa919e69e998dcee81c7a9
MarcoPenne <marco.pennese.97@gmail.com>
2019-02-18 14:54:08 +0100

    client packet functions tested

---
+++ src/client/client_packet_test_receive.c
@@ -0,0 +1,26 @@
+#include "client_packet/client_packet.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+
+    Packet *packet;
+    int fd = open("fifo", O_RDONLY);
+
+    while(1){
+
+    printf("\nWaiting for a new packet\n");
+
+
+        packet = client_receive_packet(packet, fd);
+
+
+        client_print_packet(packet);
+        free(packet);        
+        //sleep(10);
+    }
+}
---
+++ src/client/client_packet_test_sender.c
@@ -0,0 +1,43 @@
+#include "client_packet/client_packet.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+
+    Packet packet;
+    packet.type = COMMAND;
+
+    CommandPacket commandPacket;
+    commandPacket.packet = packet;
+    commandPacket.command = 1;
+
+    int pipefd[2];
+
+    int fd = open("fifo", O_WRONLY);
+
+    client_send_packet((Packet*)(&commandPacket), fd);
+
+    char buf[2]={0xaa, 0x44};
+    int ret = write(fd, buf, 2);
+    if (ret == -1) perror("Error writing");
+
+    packet.type = ERROR;
+    ErrorPacket errorPacket;
+    errorPacket.packet=packet;
+    errorPacket.error_code = UnknownCommand;
+
+    client_send_packet((Packet*)(&errorPacket), fd);
+
+    packet.type = STATUS;
+    StatusPacket statusPacket;
+    statusPacket.packet=packet;
+    statusPacket.angle=32;
+    statusPacket.distance=52.0;
+
+    client_send_packet((Packet*)(&statusPacket), fd);
+
+    sleep(20);
+}
