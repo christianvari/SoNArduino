9f3d90505fb695d3ef81d069f3eed8ff80890684
MarcoPenne <marco.pennese.97@gmail.com>
2019-03-05 23:30:13 +0100

    gtk client first attempt

---
+++ src/client/tests/gtk_test.c
@@ -0,0 +1,97 @@
+#include <cairo.h>
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <math.h>
+
+
+/**
+
+    Prerequisites:
+  
+        libgtk-3-dev
+        libcairo2-dev
+
+    Compile:
+
+        gcc gtk_test.c -o gtk_test `pkg-config --cflags --libs cairo gtk+-3.0` -lm
+
+ */
+
+
+
+static void do_drawing(cairo_t *, GtkWidget*);
+
+struct {
+  int count;
+  double coordx[100];
+  double coordy[100];
+} glob;
+
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data)
+{
+  do_drawing(cr, widget);
+  return FALSE;
+}
+
+static void do_drawing(cairo_t *cr, GtkWidget *widget)
+{
+  GtkWidget *win = gtk_widget_get_toplevel(widget);
+  
+  int width, height;
+  gtk_window_get_size(GTK_WINDOW(win), &width, &height);
+  
+  cairo_set_line_width(cr, 3);  
+  cairo_set_source_rgb(cr, 0.69, 0.19, 0);
+  
+  cairo_translate(cr, width/2, height/4*3);
+  cairo_arc(cr, 0, 0, 300, 0, M_PI*2.0);
+  cairo_stroke_preserve(cr);   
+}
+
+static gboolean clicked(GtkWidget *widget, GdkEventButton *event, gpointer user_data)
+{
+    printf("event->button: %d\n", event->button);
+    if (event->button == 1) {
+        glob.coordx[glob.count] = event->x;
+        glob.coordy[glob.count++] = event->y;
+    }
+
+    if (event->button == 3) {
+        gtk_widget_queue_draw(widget);
+    }
+
+    return TRUE;
+}
+
+
+int main(int argc, char *argv[])
+{
+  GtkWidget *window;
+  GtkWidget *darea;
+  
+  glob.count = 0;
+
+  gtk_init(&argc, &argv);
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+  darea = gtk_drawing_area_new();
+  gtk_container_add(GTK_CONTAINER(window), darea);
+ 
+  gtk_widget_add_events(window, GDK_BUTTON_PRESS_MASK);
+
+  g_signal_connect(G_OBJECT(darea), "draw", G_CALLBACK(on_draw_event), NULL); 
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);  
+    
+  //g_signal_connect(window, "button-press-event", G_CALLBACK(clicked), NULL);
+ 
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_window_set_default_size(GTK_WINDOW(window), 800, 600); 
+  gtk_window_set_title(GTK_WINDOW(window), "Lines");
+
+  gtk_widget_show_all(window);
+
+  gtk_main();
+
+  return 0;
+}
