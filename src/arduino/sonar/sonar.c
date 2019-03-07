b77f6f24b0788ba41f82499486f07950877eb930
Christian Vari <vari.christian@gmail.com>
2019-03-07 11:16:35 +0100

    sonar now working quite good

--- src/arduino/sonar/sonar.c
+++ src/arduino/sonar/sonar.c
@@ -1,107 +1,78 @@
+/**
+ * @author Christian Vari
+ * @email vari.christian@gmail.com
+ * @create date 2019-03-05 18:01:31
+ * @modify date 2019-03-05 18:01:31
+ * @desc Sonar Library
+ */
+ 
 #include "sonar.h"
+#include "../arduino_packet/uart.h"
+ 
+volatile uint32_t overFlowCounter = 0;
+ 
+void init_sonar(){
+    TRIG_DDR |= (1<<TRIG_BIT);     // Set Trigger pin as output
+    ECHO_DDR &= ~(1<<ECHO_BIT);      // Set Echo pin as input
+}
+ 
+/**
+ *
+ * Invia il segnale di trigger
+ *
+ */
+void trigger_sonar(){
+    TRIG_PORT &= ~(1<<TRIG_BIT);             // Abbasso il pin del trigger
+    _delay_us(1);                       
+    TRIG_PORT |=(1<<TRIG_BIT);               // Alzo il pin
+    _delay_us(12);                           // Tengo alto per 10us (per sicurezza 12)
+    TRIG_PORT &= ~(1<<TRIG_BIT);             // Abbasso il pin
+    _delay_us(1);                            
+}
 
-#include <stdint.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
-#include <stdio.h>
-
-
-
-uint16_t counter;
-
-// bit 0 of port C => PC0 => pin 37
-uint8_t triggers = 0x1;   
-
-// raised by isr when a falling edge of the echo is received
-// bit 0 of port K => PK0 => pin A8
-uint8_t echos = 0x1;
-
-
-/* =======  FUNCTIONS  ======= */
-
-
-/* =======  Counter  ======= */
-
-void Counter_init(void){
-  // set normal mode of operation
-  TCCR4A=0;
-  TCCR4B=(1 << CS42);
-  OCR4A=0xFFFF;
+ISR(TIMER3_OVF_vect){       // Timer1 overflow interrupt
+    overFlowCounter++;      // Tengo il conto dei timer che ho riempito
+    TCNT3=0;
 }
 
 
-/* =======  Sonar  ======= */
+uint16_t read_sonar(){
+    uint16_t dist_in_cm = 0;
+    uint32_t trig_counter = 0;
+    init_sonar();                       
+    trigger_sonar();                    
 
-void Sonar_init() {
-  Counter_init();
-  
-  DDRC=triggers;
-  // level low
-  PORTC=0x0;
-
-  DDRK=0x0;
-  // enable pull_up resistors
-  PORTK=triggers;
-    
-  // mask all echoes
-  PCMSK2 = 0;
-
-  // enable interrupts
-  PCICR|=(1<<PCIE2);
-
+    while(!(ECHO_PIN & (1<<ECHO_BIT))){     // Aspetto che il pin di echo venga alzato
+        trig_counter++;
+        if (trig_counter > SONAR_TIMEOUT){
+            return TRIG_ERROR;
+        }
+    }
+ 
+    TCNT3=0;                            // resetto il timer
+    TCCR3B |= (1<<CS30);                // avvio il timer senza prescaler
+    TIMSK3 |= (1<<TOIE3);               // attivo l'interrupt overflow sul timer 3
+    overFlowCounter=0;                  // resetto il contatore di overflow
+    sei();                              // attivo gli interrupt
+ 
+    while((ECHO_PIN & (1<<ECHO_BIT))){    // Aspetto che il pin venga abbassato
+        if (((overFlowCounter*TIMER_MAX)+TCNT3) > SONAR_TIMEOUT){
+            return ECHO_ERROR;            // L'oggetto è fuori dal range massimo
+        }
+    };
+ 
+    TCCR3B = 0x00;                      // fermo il timer
+    cli();                              // disabilito gli interrupt
+    dist_in_cm = (((overFlowCounter*TIMER_MAX)+TCNT3)/(TO_CM*ISTRUZIONI_US));   // distance in cm
+    return (dist_in_cm );
 }
 
+int main(void){
 
-uint8_t sonar_ping(void){
-  // if a reading is in progress return a failure
-  /* echos=PINC; */
-  /* if(echos) */
-  /*   return 0; */
-
-  // clear timers
-  
-  counter = 0;
-  
-  // send a trigger and wait 10 us
-  PORTC=triggers;
-  _delay_us(10);
-  PORTC=0;
-
-  TCNT4 = 0;
-
-  sei();
-  // enable interrupt on change for selected sonars
-  PCMSK2=1<<PCINT16;
-  cli();
-  return 1;
-}
-
-uint8_t sonar_get_last(void){
-  return counter;
-}
-
-ISR(PCINT2_vect) {
-  uint16_t current_time=TCNT4;
-  uint8_t current_echo=PINK;
-
-  cli();
-  printf("interrupt");
-
-  // look only at the current bit
-  uint8_t mask=0x1;
-
-  // if nothing happened on that sonar, we skip
-  if ((current_echo&mask)==(echos&mask))
-    return;
-
-  // in case of a rising  edge, we save the timer
-  if (current_echo&mask) {
-    counter = current_time;
-  } else {
-  // otherwise we put in the counters[s] the duration (echo end)
-    counter = current_time-counter;
+  printf_init();
+  while(1){
+        printf("distanza %d\n",read_sonar());
+        _delay_ms(1000);
   }
   
-  echos=current_echo;
-  sei();
 }
