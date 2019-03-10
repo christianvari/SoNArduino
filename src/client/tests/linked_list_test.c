6a00023b158de652fcf9343b750da07725f3327e
MarcoPenne <marco.pennese.97@gmail.com>
2019-03-10 22:34:25 +0100

    changing in list test

--- src/client/tests/linked_list_test.c
+++ src/client/tests/linked_list_test.c
@@ -8,67 +8,41 @@ int main(){
     head = malloc(sizeof(ListHead));
     List_init(head, 4);
 
-    ListItem* i=malloc(sizeof(ListItem));
-    i->next=NULL;
-    i->prev=NULL;
-    i->angle=1;
-    i->distance=1;
-    List_insert(head, i);
+    List_insert(head, 1, 1);
     
     List_print(head);
     List_print_reverse(head);
     printf("-----\n");
 
-    i=malloc(sizeof(ListItem));
-    i->next=NULL;
-    i->prev=NULL;
-    i->angle=2;
-    i->distance=2;
-    List_insert(head, i);
+
+    List_insert(head, 2,2);
 
     List_print(head);
     List_print_reverse(head);
     printf("-----\n");
 
-    i=malloc(sizeof(ListItem));
-    i->next=NULL;
-    i->prev=NULL;
-    i->angle=3;
-    i->distance=3;
-    List_insert(head, i);
 
-    List_print(head);
-    List_print_reverse(head);
-    printf("-----\n");
-    
-    i=malloc(sizeof(ListItem));
-    i->next=NULL;
-    i->prev=NULL;
-    i->angle=4;
-    i->distance=4;
-    List_insert(head, i);
+    List_insert(head, 3,3);
 
     List_print(head);
     List_print_reverse(head);
     printf("-----\n");
 
-    i=malloc(sizeof(ListItem));
-    i->next=NULL;
-    i->prev=NULL;
-    i->angle=5;
-    i->distance=5;
-    List_insert(head, i);
+    List_insert(head, 4,4);
 
     List_print(head);
     List_print_reverse(head);
     printf("-----\n");
 
-    i=malloc(sizeof(ListItem));
-    i->next=NULL;
-    i->prev=NULL;
-    i->angle=6;
-    i->distance=6;
-    List_insert(head, i);
+
+    List_insert(head, 5,5);
+
+    List_print(head);
+    List_print_reverse(head);
+    printf("-----\n");
+
+
+    List_insert(head, 6,6);
 
     List_print(head);
     List_print_reverse(head);
