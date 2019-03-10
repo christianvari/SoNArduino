87cd08cde167f66e4795f5590f8cfdcfe932beb6
MarcoPenne <marco.pennese.97@gmail.com>
2019-03-10 16:15:36 +0100

    implemented a simple checksum function

--- src/packet/packet.c
+++ src/packet/packet.c
@@ -2,5 +2,11 @@
 
 
 uint8_t calculate_checksum(uint8_t *buf, int n){
-    return 0;
+      
+      uint8_t cs=0;
+      int i;
+
+      for (i = 0; i < n; ++i)
+            cs += (unsigned int)(*buf++);
+      return cs;
 }
